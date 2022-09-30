4abf299c9dc139aaae0a02e29afba41b0f9f241a
unknown <helderhenderson143@gmail.com>
2022-09-30 00:22:57 -0300

    FIX : config dotenv-safe, and change env to process.env

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,10 @@
 import server from "./server";
-import * as env from "dotenv";
 import path from "./core/routes/path";
-const dotenv = env.config({ path: __dirname + "/.env" }).parsed;
-
-const PORT = dotenv?.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log(`[server]: Server is running at PORT:${PORT}`);
   console.log(
-    `[server]: See swagger in http://localhost:${PORT}${path.swagger}`
+    `[server]: See swagger in -->>>  http://localhost:${PORT}/${path.swagger} <<<--`
   );
 });
